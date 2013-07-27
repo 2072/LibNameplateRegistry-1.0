8b97c779820b45bea5468f90d9c37e2104c6503a
John Wellesz <archarodim@teaser.fr>
2013-07-27 23:11:30 +0200

    Basic empty library skeleton

---
+++ LibNameplateRegistry-1.0/LibNameplateRegistry-1.0.lua
@@ -0,0 +1,27 @@
+--[[
+        LibNameplateRegistry-1.0
+        Copyright (c) 2013 by John Wellesz (Archarodim@teaser.fr)
+        
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser Public License for more details.
+
+    You should have received a copy of the GNU Lesser Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--]]
+
+
+
+local MAJOR, MINOR = "LibNameplateRegistry-1.0", 1
+
+local NameplateRegistry = LibStub:NewLibrary(MAJOR, MINOR)
+if not NameplateRegistry then return end
+
+
